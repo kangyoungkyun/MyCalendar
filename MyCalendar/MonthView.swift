bd51e6c9f10eea4c03a1331543ab0febdc8ef002
MacBookPro <macbookpro@MacBookProui-MacBook-Pro.local>
2018-04-11 16:15:44 +0900

    MonthView , 월 과 이전달 이후달 표시해주는 뷰 제작

---
+++ MyCalendar/MonthView.swift
@@ -0,0 +1,106 @@
+//
+//  MonthView.swift
+//  MyCalendar
+//
+//  Created by MacBookPro on 2018. 4. 11..
+//  Copyright © 2018년 MacBookPro. All rights reserved.
+//
+
+import UIKit
+
+
+protocol MonthViewDelegate:class {
+    func didChangeMonth(monthIndex: Int, year: Int)
+}
+
+class MonthView: UIView {
+    
+    //년 , 월 타이틀
+    let lblName : UILabel = {
+      let lbl = UILabel()
+        lbl.text = "0000년 00월"
+        lbl.textColor = UIColor.black
+        lbl.textAlignment = .center
+        lbl.font = UIFont.boldSystemFont(ofSize: 16)
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        return lbl
+    }()
+    
+    //이전달
+    let btnLeft:UIButton = {
+      let btn = UIButton()
+        btn.setTitle("이전달", for: .normal)
+        btn.setTitleColor(UIColor.black, for: .normal)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.addTarget(self, action: #selector(btnLeftRightAction(sender:)), for: .touchUpInside)
+        btn.setTitleColor(UIColor.lightGray, for: .disabled)
+        return btn
+    }()
+    
+    //다음달
+    let btnRight:UIButton = {
+        let btn = UIButton()
+        btn.setTitle("다음달", for: .normal)
+        btn.setTitleColor(UIColor.black, for: .normal)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.addTarget(self, action: #selector(btnLeftRightAction(sender:)), for: .touchUpInside)
+        btn.setTitleColor(UIColor.lightGray, for: .disabled)
+        return btn
+    }()
+    
+    //이전달 다음달
+    @objc func btnLeftRightAction(sender: UIButton) {
+        
+        print("이전달 다음달 버튼이 눌렀습니다.")
+    }
+    
+    
+    //달의 값을 가지고 있는 배열
+    var monthsArr = ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"]
+    //보여지는 월
+    var currentMonthIndex = 0
+    //보여지는 년
+    var currentYear: Int = 0
+    
+    var delegate: MonthViewDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.blue
+        setupViews()
+    }
+    
+    
+    func setupViews() {
+        
+        
+        self.addSubview(btnLeft)
+        btnLeft.topAnchor.constraint(equalTo: topAnchor).isActive=true
+        btnLeft.leftAnchor.constraint(equalTo: leftAnchor).isActive=true
+        btnLeft.widthAnchor.constraint(equalToConstant: 50).isActive=true
+        btnLeft.heightAnchor.constraint(equalTo: heightAnchor).isActive=true
+        
+        
+        self.addSubview(lblName)
+        lblName.topAnchor.constraint(equalTo: topAnchor).isActive=true
+        lblName.centerXAnchor.constraint(equalTo: centerXAnchor).isActive=true
+        lblName.widthAnchor.constraint(equalToConstant: 150).isActive=true
+        lblName.heightAnchor.constraint(equalTo: heightAnchor).isActive=true
+        lblName.text="\(currentYear)년 \(monthsArr[currentMonthIndex])"
+        
+        self.addSubview(btnRight)
+        btnRight.topAnchor.constraint(equalTo: topAnchor).isActive=true
+        btnRight.rightAnchor.constraint(equalTo: rightAnchor).isActive=true
+        btnRight.widthAnchor.constraint(equalToConstant: 50).isActive=true
+        btnRight.heightAnchor.constraint(equalTo: heightAnchor).isActive=true
+        
+      
+    }
+    
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ MyCalendar/WeekdaysView.swift
@@ -0,0 +1,21 @@
+//
+//  WeekdaysView.swift
+//  MyCalendar
+//
+//  Created by MacBookPro on 2018. 4. 11..
+//  Copyright © 2018년 MacBookPro. All rights reserved.
+//
+
+import UIKit
+
+class WeekdaysView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
